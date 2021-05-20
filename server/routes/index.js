2930498df1ed00d401143ee77a4091985a0cfa9e
ndmnhat2000 <ndmnhat2000@gmail.com>
2021-05-20 07:24:14 +0700

    Sửa lỗi routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var appRoot = require('app-root-path');
 
 /* GET home page. */
 router.get('/*', function (req, res, next) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(appRoot.path, 'public', 'index.html'));
 });
 
 module.exports = router;
