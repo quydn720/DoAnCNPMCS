d896db4b273d48a282ca02f54cf02d076608939e
tinpham1510 <tinpham15102000@gmail.com>
2021-06-05 11:42:34 +0700

    fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,10 +14,59 @@ import Customer from './components/Customer_page/Customer';
 import Header_auth from './components/Header_auth';
 import axios from 'axios';
 import { Component } from 'react';
-import { extend } from 'jquery';
-function App(){
-    //const [success,setSuccess]=useState(false);
-    //const [isLogin,setIslogin] = useState(localStorage.getItem("access_token")!=null);
+function App(props) {
+    const [success,setSuccess]=useState(false);
+    const [account,setAccount]= useState([]);
+
+    useEffect(()=>{
+      async function Data(){
+      const requestUrl = `http://localhost:3001/api/nguoi-dung/thong-tin`;
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", "Bearer" + localStorage.getItem('access_token'));
+
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      fetch(requestUrl, requestOptions)
+        .then(response =>{ 
+            return response.json()
+        })
+        .then(result => {
+          console.log(result)
+          setAccount({
+            account: result
+          })
+        })
+        .catch(error => console.log('error', error));
+      }
+      Data();
+    },[])
+    //const Login = e =>{
+    // e.preventDefault();
+    //     fetch('http://localhost:3001/api/auth/login',{
+    //         method: "POST",
+    //         headers: {
+    //             "Accept" : "application/json",
+    //             "Content-Type" : "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //             "ten_tai_khoan": document.getElementById('ten_tai_khoan').value,
+    //             "mat_khau" : document.getElementById('mat_khau').value
+    //         })
+    //     }).then((result ) =>{
+    //         result.json().then((resp)=>{
+    //             console.log(resp.success);
+    //             setSuccess({
+    //                 success: resp.success
+    //             })
+    //         })
+    //     })
+
+    // };
+
     return ( 
         <Router>
       <div>
