62c7560bff82c24b424178c5be85e61ea86cb395
ndmnhat2000 <ndmnhat2000@gmail.com>
2021-06-07 17:38:42 +0700

    sửa cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var fileUpload = require('express-fileupload');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 var { swaggerUI, swaggerDocs } = require('./config/swagger-config');
 
 var indexRouter = require('./routes/index');
@@ -13,13 +14,13 @@ require('dotenv').config();
 
 var app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //res.header('Access-Control-Allow-Methods', ' PUT, POST, GET, DELETE, OPTIONS');
-    next();
-  });
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //res.header('Access-Control-Allow-Methods', ' PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+app.use(cors({ credentials: true }));
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(logger('dev'));
